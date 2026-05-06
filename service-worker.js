ea6e313d8e895ac60eb3d49ff845de2cf0a08b5e
Po <poyingchan21@gmail.com>
2026-05-06 22:52:42 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sid-calculator-v1';
+const CACHE_NAME = 'sid-calculator-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
